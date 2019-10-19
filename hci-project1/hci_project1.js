64e94661f4aa5cb63ee4d3c2816bae39d80e974f
Michael Bishop <michael.bishop@ttu.edu>
2019-10-19 15:13:57 -0500

    Image Creation; Website created;

--- hci-project1/hci_project1.js
+++ hci-project1/hci_project1.js
@@ -1,8 +1,36 @@
-function setup() {
+// function setup() {
 
+// }
+
+
+// function draw() {
+
+// }
+
+function switchNews() {
+   
 }
 
+function switchStock() {
+    document.getElementById("body").style.setProperty("--back-url", "url(\"assets/Sketch-selectStock-1.jpg\")");
+}
 
-function draw() {
+function switchNotification() {
+    document.getElementById("body").style.setProperty("--back-url", "url(\"assets/Sketch-selectStock-1.jpg\")");
+}
 
+function switchLight() {
+    document.getElementById("body").style.setProperty("--back-url", "url(\"assets/Sketch-selectStock-1.jpg\")");
+}
+
+function switchGraph() {
+    document.getElementById("body").style.setProperty("--back-url", "url(\"assets/Sketch-selectStock-1.jpg\")");
+}
+
+function switchDrag() {
+    document.getElementById("body").style.setProperty("--back-url", "url(\"assets/Sketch-selectStock-1.jpg\")");
+}
+
+function switchBase() {
+    document.getElementById("body").style.setProperty("--back-url", "url(\"assets/Sketch-selectStock-1.jpg\")");
 }
