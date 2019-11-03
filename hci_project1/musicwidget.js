3bef7f654155beddf5bd844026ee7734558f4e4e
The_Fireplace <the.f1repl4ce@gmail.com>
2019-11-03 16:17:50 -0600

    Fix continually setting HTML for play/pause on music widget

--- hci_project1/musicwidget.js
+++ hci_project1/musicwidget.js
@@ -24,12 +24,8 @@ function musicWidgetDraw(x, y, w, h) {
   nextButton.position(x+w-18, y+h/2);
   //turn play/pause buttons on and off as needed
   if(!playing) {
-    pauseButton.html('', false);
-    playButton.html("<i class='fas fa-play'></i>", false);
     playButton.position(x+w/2-9, y+h/2);
   } else {
-    playButton.html('', false);
-    pauseButton.html("<i class='fas fa-pause'></i>", false);
     pauseButton.position(x+w/2-9, y+h/2);
     if(curTimeStamp < maxTimeStamp) {
       curTimeStamp++;
@@ -47,6 +43,13 @@ function musicWidgetDraw(x, y, w, h) {
 function musicWidgetClick(x, y, w, h) {
   if(Math.abs(w/2-x) <= 8 && Math.abs(h/2+6-y) <= 8) {
     playing = !playing;
+    if(!playing) {
+      pauseButton.html('', false);
+      playButton.html("<i class='fas fa-play'></i>", false);
+    } else {
+      playButton.html('', false);
+      pauseButton.html("<i class='fas fa-pause'></i>", false);
+    }
   }
 }
 
